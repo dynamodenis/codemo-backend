99198f4e02dd38d3e8d9c6e8d4c97593ed122fd7
dynamodenis <dmbugua66@gmail.com>
2020-07-02 10:36:31 +0300

    initial deploy

--- hackerRank/settings.py
+++ hackerRank/settings.py
@@ -15,6 +15,7 @@ from datetime import timedelta
 import os
 import dj_database_url
 from decouple import config,Csv
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -30,7 +31,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-
+ALLOWED_HOSTS = []
 
 CORS_ORIGIN_ALLOW_ALL=True
 ACCESS_KEY = config('ACCESS_KEY')
